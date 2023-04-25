b0bf7bfdf70dfe8ff38d3819eb664ed522953714
Rishabh Tiwari <127232249+RIshbahTiwari@users.noreply.github.com>
2023-04-25 21:40:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+# description before using the code
+
+#before using this make sure you already have intall these two extensions :-
+#1- pip install shortner
+#2-pip install pyperclip
+#for intalling these extensions use the followoing steps
+#paste this line in terminal of your ide (ex-vscode,pycharm) :-
+# 1st line ------>  pip install shortner  
+# after this hit enter and then copy this and paste in terminal
+#2nd line ---------> pip install pyperclip
+# hit enter, and then use below code
+
+
+# main code
+#------->>>>
+
 import pyshorteners
 
 url = input("Enter the url \n: ")
